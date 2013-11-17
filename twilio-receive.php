08c7720fd1471e0b0171bc4885b07d9bf5f90e10
James Wei <wei.c.james@gmail.com>
2013-11-17 01:43:27 -0500

    Initial implementation of twilio receive

--- twilio-receive.php
+++ twilio-receive.php
@@ -1,3 +1,38 @@
 <?php
-  echo "Hello there!";
+  
+  // db vars
+  $dbHost = "us-cdbr-azure-west-b.cleardb.com";
+  $dbUser = "bcd4a2c313611e";
+  $dbPass = "886d7131";
+  $dbName = "hackdukedatabase";
 
+  // msg vars
+  $from = $_REQUEST['From'];
+  $body = $_REQUEST['Body'];
+  
+  // get student from db
+  $mysqlCon = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+  if (mysqli_connect_errno($mysqlCon)) {
+    $responseMessage = "Sorry, an error occurred in storing your response :(";
+    exit("Error connecting to db");
+  }
+  $fromNormalized = substr($from, 2);
+  $result = mysqli_query($mysqlCon, "SELECT StudentId FROM student WHERE PhoneNumber=$fromNormalized");
+  if ($result == NULL) {
+    exit("Received message from non-registered number");
+  }
+  $studentId = mysqli_fetch_array($result);
+  $responseExists = mysqli_query($mysqlCon, "SELECT Response FROM response WHERE StudentId=$studentId") != NULL;
+  if ($responseExists) {
+    mysqli_query($mysqlCon, "UPDATE response SET Response=$body WHERE StudentId=$studentId");
+  } else {
+    mysqli_query($mysqlCon, "INSERT INTO response (StudentId, Response) VALUES ($studentId, $body)");
+  }
+  
+?>
+
+<Response>
+  <Message>
+    Thanks, we received your response of: <?php echo $body ?>
+  </Message>
+</Response>

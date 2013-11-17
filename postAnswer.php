c21865ff07873d640825c133d6244d7bd075bdd8
eldermyoshida <elder.yoshida@gmail.com>
2013-11-17 02:41:38 -0500

    Changed exit and var dump

--- postAnswer.php
+++ postAnswer.php
@@ -6,7 +6,8 @@
 	} else {
 		//Bad Request
 		printf("Connect failed: %s\n", mysqli_connect_error());
-		exit(406);	
+		http_status_code(406);
+		exit(1);	
 	}
 	
 	if(isset($_GET['Phone'])){
@@ -14,21 +15,26 @@
 		$std_phone = filter_var(@$_GET['Phone'], FILTER_SANITIZE_NUMBER_INT);
 		$query = "SELECT student.StudentId FROM hackdukedatabase.student WHERE student.PhoneNumber = '{$std_phone}'";
 		$std_id = $mysqli->query($query) or die($mysqli->error.__LINE__);		
+		var_dump($std_id);
+		$std_id->free();
 	} elseif (isset($_GET['Email'])){
 		//SendGrid
 		$std_email = filter_var(@$_GET['Email'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
 		$query = "SELECT student.StudentId FROM hackdukedatabase.student WHERE student.Email = '{$std_email}'";
 		$std_id = $mysqli->query($query) or die($mysqli->error.__LINE__);
+			var_dump($std_id);
+		$std_id->free();
 	} else{
 		//Bad request
-		exit(406);
+		http_status_code(406);
+		exit(1);
 	}	
 	$query = "INSERT INTO response (ClassId, StudentId, Response) SELECT classlog.ClassId, '{$std_id}', '{$std_resp}' WHERE classlog.StudentId = '{$std_id}' FROM hackdukedatabase.classlog ORDER BY TimeStarted DESC LIMIT 1";
 	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 	
 	if($result==TRUE){
-		echo true;
-		exit(202);
+		http_status_code(202);
+		exit(1);
 	}
 	
 	$result->free();

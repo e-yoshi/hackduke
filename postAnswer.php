de9d4b0ca0675102790f0e0e79fe926e3fa8fe84
eldermyoshida <elder.yoshida@gmail.com>
2013-11-17 03:34:20 -0500

    boolean logic

--- postAnswer.php
+++ postAnswer.php
@@ -35,7 +35,7 @@
 		http_status_code(406);
 		exit(1);
 	}	
-	if(is_null($std_id)){
+	if(!is_null($std_id)){
 	$query = "INSERT INTO response (ClassId, StudentId, Response) SELECT classlog.ClassId, '{$std_id}', '{$std_resp}' FROM hackdukedatabase.classlog WHERE classlog.StudentId='{$std_id}' ORDER BY TimeStarted DESC LIMIT 1";
 	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 	var_dump($result);

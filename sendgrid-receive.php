c5f33c7b05719a7f3754b24dc431ea9948e37fef
xurui203 <xurui203@gmail.com>
2013-11-17 07:51:30 -0500

    test2

--- sendgrid-receive.php
+++ sendgrid-receive.php
@@ -13,8 +13,8 @@ ini_set('display_errors', '1');
   $dbName = "hackdukedatabase";
 
  // email vars
-  $from = $_POST['From'];
-  $body = $_POST['Text'];
+  $from = $REQUEST['From'];
+  $body = $REQUEST['Text'];
  
   // get student from db
   $mysqlCon = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);

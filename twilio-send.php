4b14fb70e1f5a02bda7a491e7d141b1cdbf9a085
James Wei <wei.c.james@gmail.com>
2013-11-17 11:29:23 -0500

    SDLJF

--- twilio-send.php
+++ twilio-send.php
@@ -38,11 +38,11 @@ error_reporting(E_ALL);
   //$query = "INSERT INTO quiz (Question, ClassId) VALUES ('$question', '$classId')";
   //$result = mysqli_query($mysqlCon, $query);
   //$result->free();
-  $query = "DELETE FROM hackdukedatabase.response";
-  $result = mysqli_query($mysqlCon, $query);
-  @$result->free();
-  echo "True";
-  $mysqlCon->close();
+  //$query = "DELETE FROM hackdukedatabase.response";
+  //$result = mysqli_query($mysqlCon, $query);
+  //@$result->free();
+  //echo "True";
+  //$mysqlCon->close();
 
   // send text messages
   $twilioPhone = "919-666-3358";

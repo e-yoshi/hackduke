77addbbe6c5c6926a7fe2aef47dc83223c0cc25c
xurui203 <xurui203@gmail.com>
2013-11-17 00:22:09 -0500

    added credentials

--- sendemails.php
+++ sendemails.php
@@ -1,7 +1,8 @@
 <?php
 include 'path/to/sendgrid-php/SendGrid_loader.php';
-$sendgrid = new SendGrid('username', 'password');
+$sendgrid = new SendGrid('azure_0c3c5610525389e9875c7eda6e42678a@azure.com', '7rjwkvyt');
 $mail = new SendGrid\Mail();
+echo "new sendgrid mail created";
 $mail->
   addTo('jcw46@duke.edu')->
   setFrom('xurui203@gmail.com')->
@@ -9,6 +10,6 @@ $mail->
   setText('Here is your $20!')->
   setHtml('<strong>Here is your $20!</strong>');
 $sendgrid->
-web->
+smtp->
   send($mail);
 ?>

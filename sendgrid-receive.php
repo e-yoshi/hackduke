eddeef3233ed6907216ab023dbb01c132d4f33dd
xurui203 <xurui203@gmail.com>
2013-11-17 06:40:25 -0500

    check

--- sendgrid-receive.php
+++ sendgrid-receive.php
@@ -3,7 +3,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-var_dump($_POST);
+
+echo "from: $_POST['from']";
+
+echo "to: $_POST['to']";
+
+echo "subject: $_POST['subject']";
+
+echo "text: $_POST['text']";
 
 
 ?>

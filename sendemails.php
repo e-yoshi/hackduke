137536f229bfeb06736d650b383219201a28edd4
xurui203 <xurui203@gmail.com>
2013-11-17 00:29:57 -0500

    changed email fields

--- sendemails.php
+++ sendemails.php
@@ -4,8 +4,8 @@ $sendgrid = new SendGrid('azure_0c3c5610525389e9875c7eda6e42678a@azure.com', '7r
 $mail = new SendGrid\Mail();
 echo "new sendgrid mail created";
 $mail->
-  addTo('jcw46@duke.edu')->
-  setFrom('xurui203@gmail.com')->
+  addTo('elder.yoshida@gmail.com')->
+  setFrom('7rjwkvyt@sendgrid.com')->
   setSubject('Payment')->
   setText('Here is your $20!')->
   setHtml('<strong>Here is your $20!</strong>');

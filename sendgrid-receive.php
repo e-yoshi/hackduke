d0eee1f5434cd473efb3f363b7d920ef481b29e2
James Wei <wei.c.james@gmail.com>
2013-11-17 07:04:32 -0500

    sendgrid receive updates db

--- sendgrid-receive.php
+++ sendgrid-receive.php
@@ -3,22 +3,44 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-  // require the twilio php library 
-  require "twilio-php-master/Services/Twilio.php";
+  // db vars
+  $dbHost = "us-cdbr-azure-west-b.cleardb.com";
+  $dbUser = "bcd4a2c313611e";
+  $dbPass = "886d7131";
+  $dbName = "hackdukedatabase";
 
-  // create twilio client
-  $AccountSid = "AC3d59d01368bbc628721a869c0404786b";
-  $AuthToken = "07284eb8cf827621a327022ba0bd47b4";
-  $client = new Services_Twilio($AccountSid, $AuthToken);
-
-$from = $_POST['from'];
-$to = $_POST['to'];
-$subject = $_POST['subject'];
-$body = $_POST['text'];
-
-  $twilioPhone = "919-666-3358";
-$phone = "9198109361";    
-$sms = $client->account->messages->sendMessage($twilioPhone, $phone, "From: $from; To: $to; Subject: $subject; Body: $body");
+ // email vars
+  $from = $_POST['from'];
+  $body = $_POST['test'];
+ 
+  // get student from db
+  $mysqlCon = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+  if (mysqli_connect_errno($mysqlCon)) {
+    $messageResponse = "Sorry, an error occurred in storing your response :(";
+    exit("Error connecting to db");
+  }
+  $result = mysqli_query($mysqlCon, "SELECT StudentId FROM student WHERE Email=$from");
+  if ($result == NULL) {
+    exit("Received message from non-registered email");
+  }
+  
+  $std_resp = filter_var($body, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+  $std_email = filter_var($from, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+  $query = "SELECT student.StudentId FROM hackdukedatabase.student WHERE student.Email='{$std_email}'";
+  $result = mysqli_query($mysqlCon, $query);
+  $rows = mysqli_fetch_array($result);
+  $std_id = $rows[0];
+  if (!is_null($std_id)) {
+    $query = "INSERT INTO response (ClassId, StudentId, Response) SELECT classlog.ClassId, '{$std_id}', '{$std_resp}' FROM hackdukedatabase.classlog WHERE classlog.StudentId='{$std_id}' ORDER BY TimeStarted DESC LIMIT 1";
+    $result = mysqli_query($mysqlCon, $query);
+    if ($result == TRUE) {
+      $messageResponse = "Successfully saved your response to the db! Response was: $body";
+    } else {
+      $messageResponse = "Could not update the db :(";
+    }
+  } else {
+    $messageResponse = "Could not find student id for email";
+  }	
 
 ?>
 

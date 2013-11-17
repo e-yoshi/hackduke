8a13c90fd43cf54db782b4365e9439046e7f37f2
eldermyoshida <elder.yoshida@gmail.com>
2013-11-17 02:28:35 -0500

    Changed post queries to support latest classes Id

---
+++ getAnswers.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once ('db.php');
+	
+	
+/*
+// A QUICK QUERY ON A FAKE USER TABLE
+	$query = "SELECT * FROM `users` WHERE `status`='bonkers'";
+	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+// GOING THROUGH THE DATA
+	if($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			echo stripslashes($row['username']);	
+		}
+	}
+	else {
+		echo 'NO RESULTS';	
+	}
+	*/
+?>
--- query.php
+++ query.php
@@ -1,5 +1,10 @@
 <?php  
 	if($_POST['Query']=='CreateQuestion') {
-		echo "aself";
+		$teacher_name = filter_var($_GET['TeacherName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$teacher_password = filter_var($_GET['Password'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+			
+			
+			filter_var(@$_GET['ClassId'], FILTER_SANITIZE_NUMBER_INT);
+
 	}
 ?>

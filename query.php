fac4760e4fc484ea5340f1d7681e94ce4bc572f3
James Wei <wei.c.james@gmail.com>
2013-11-17 02:56:25 -0500

    Added test connection to query php

--- query.php
+++ query.php
@@ -1,10 +1,16 @@
 <?php  
-	if($_POST['Query']=='CreateQuestion') {
-		$teacher_name = filter_var($_GET['TeacherName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-		$teacher_password = filter_var($_GET['Password'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-			
-			
-			filter_var(@$_GET['ClassId'], FILTER_SANITIZE_NUMBER_INT);
+  
+  $queryType = $_POST['Query'];
 
-	}
+  if ($queryType == 'TestConnection') {
+    echo "TRUE";
+  }
+
+  if($_POST['Query']=='CreateQuestion') {
+    $teacher_name = filter_var($_GET['TeacherName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+    $teacher_password = filter_var($_GET['Password'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+			
+    filter_var(@$_GET['ClassId'], FILTER_SANITIZE_NUMBER_INT);
+
+  }
 ?>

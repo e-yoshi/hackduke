afe88307c196ae5549af790156aed29bdc43b629
xurui203 <xurui203@gmail.com>
2013-11-17 02:20:28 -0500

    shld work...

--- sendemails.php
+++ sendemails.php
@@ -2,23 +2,38 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require 'sendgrid-php/SendGrid_loader.php';
+ $url = 'http://sendgrid.com/';
+ $user = 'azure_0c3c5610525389e9875c7eda6e42678a@azure.com';
+ $pass = '7rjwkvyt';
 
-$sendgrid = new SendGrid('azure_0c3c5610525389e9875c7eda6e42678a@azure.com', '7rjwkvyt');
-$mail = new SendGrid\Mail();
-echo "new sendgrid mail created";
-$mail->
-  addTo('xurui203@gmail.com')->
-  addTo('nick@sendgrid.com')->
-  setFrom('xurui203@gmail.com')->
-  setSubject('Payment')->
-  setText('Here is your $20!')->
-  setHtml('<strong>Here is your $20!</strong>');
-$work = $sendgrid->smtp->send($mail);
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => 'jcw46@duke.edu',
+      'subject' => 'Your payment request has been rejected.',
+      'html' => 'yolo',
+      'text' => 'yolo',
+      'from' => 'xurui203@gmail.com',
+   );
 
+ $request = $url.'api/mail.send.json';
 
-echo $work ? "YAY!" : "nay.";
+ // Generate curl request
+ $session = curl_init($request);
 
-echo "new sendgrid mail created";
+ // Tell curl to use HTTP POST
+ curl_setopt ($session, CURLOPT_POST, true);
 
-?>
+ // Tell curl that this is the body of the POST
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // Tell curl not to return headers, but do return the response
+ curl_setopt($session, CURLOPT_HEADER, false);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ print_r($response);

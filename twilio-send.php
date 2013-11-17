7b2f218327395ab8c02f474f674d7a17d9f21595
James Wei <wei.c.james@gmail.com>
2013-11-17 09:12:22 -0500

    Updated student id column name in twilio send

--- twilio-send.php
+++ twilio-send.php
@@ -28,17 +28,17 @@ error_reporting(E_ALL);
   }
 
   // make query
-  $studentIds = mysqli_fetch_array(mysqli_query($mysqlCon, "SELECT StudentId FROM class WHERE ClassId=$classId"));
+  $studentIds = mysqli_fetch_array(mysqli_query($mysqlCon, "SELECT StudentIds FROM class WHERE ClassId=$classId"));
   if ($studentIds == NULL) {
     exit("Class not found");
   }
-  $studentIdArray = explode(",", $studentIds['StudentId']);
+  $studentIdArray = explode(",", $studentIds['StudentIds']);
 
   // send text messages
   $twilioPhone = "919-666-3358";
   foreach ($studentIdArray as $id) {
     echo "Getting phone for student $id<br>";
-    $phone = mysqli_fetch_array(mysqli_query($mysqlCon, "SELECT PhoneNumber FROM student WHERE StudentId=$id"))['PhoneNumber'];
+    $phone = mysqli_fetch_array(mysqli_query($mysqlCon, "SELECT PhoneNumber FROM student WHERE StudentIds=$id"))['PhoneNumber'];
     $sms = $client->account->messages->sendMessage($twilioPhone, $phone, "Hello from Inquizio! Your instructor has requested a response; please reply to this text with the letter corresponding to your answer!");
     echo "Sent message to student $id at $phone<br>";
   }

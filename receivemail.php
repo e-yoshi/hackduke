97e480e04cc8989406383bc31ea844134b81b6f5
xurui203 <xurui203@gmail.com>
2013-11-17 04:22:23 -0500

    print data

--- receivemail.php
+++ receivemail.php
@@ -3,19 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-$fh = fopen('~/parse.log', 'a+');
-if ( $fh )
-{
-        fwrite($fh, print_r($_POST, true) . print_r($_FILES, true));
-        foreach ($_FILES as $key => $file)
-        {
-                move_uploaded_file($file['tmp_name'], "/tmp/".$file['name']);
-        }
-        fclose($fh);
-}
-echo "done";
-
-
+var_dump($_POST);
 
 
 ?>

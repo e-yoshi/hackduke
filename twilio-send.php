4d3d248d6b1f84b6e2cf50feb3a69ab00150a84c
eldermyoshida <elder.yoshida@gmail.com>
2013-11-17 11:51:58 -0500

    adsasdasd

--- twilio-send.php
+++ twilio-send.php
@@ -38,10 +38,10 @@ error_reporting(E_ALL);
   //$query = "INSERT INTO quiz (Question, ClassId) VALUES ('$question', '$classId')";
   //$result = mysqli_query($mysqlCon, $query);
   //$result->free();
-  $query = "DELETE FROM hackdukedatabase.response";
+  $query = "DELETE * FROM hackdukedatabase.response";
   $result = mysqli_query($mysqlCon, $query);
-  @$result->free();
-  echo "True";
+  $result->free();
+  echo TRUE;
   //
 
   // send text messages
